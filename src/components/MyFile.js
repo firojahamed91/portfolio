99da7e13c5a027dbf5a5da4b81d5fb3297be5854
firojahamed91 <firojahamed91@gmail.com>
2021-09-19 00:34:54 +0600

    first commit

---
+++ src/components/MyFile.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.css'
+import airplane from './images/airplane.png'
+
+const MyFile = () => {
+    return (
+        <>
+            <div class="container">
+                <div class="runway"></div>
+                <img src={airplane} class="airplane"/>
+            </div>
+        </>
+    )
+}
+
+export default MyFile
